95c7f6af44e005f01164492629421966ed69f29b
Ankit Sharma <an2ki4t5s@gmail.com>
2023-07-06 19:35:34 +0530

    my approach using maps

---
+++ Array/pairs.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> arr = {10,2,3,5,-6,9,11};
+    vector<int> pair; 
+    unordered_map<int,int> map;
+    int tar = 4;
+
+    for(int i : arr){
+        int x = 0;
+        map[i] = x;
+        x++;
+    }
+    
+    for(int i = 0; i < arr.size(); i++){
+        int ser = 0;
+        if(arr[i] > tar){
+            ser = arr[i] - tar;
+            if(map.find(ser) != map.end()){
+                pair.push_back(arr[i]);
+                pair.push_back(arr[i]-tar);
+            }
+
+            if (arr[i] < tar){
+            ser = tar - arr[i];
+            if(map.find(ser) != map.end()){
+                pair.push_back(arr[i]);
+                pair.push_back(tar - arr[i]);
+            }
+            
+        }
+    }
+
+}
+
+for(int i : pair){
+    cout<<i<<" ";
+}
+
+return 0;
+}
+
